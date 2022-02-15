932d0fae8e8371ac9dc494472eae4211da59d2bd
ahhongz89 <ahhongz89@gmail.com>
2022-02-16 00:16:26 +0800

    updated gitignore to ignore /test-output/ folder, added junit dependency added junit5 and testng integration dependencies in pom.xml. Added UserTest file for junit testing

---
+++ src/test/java/first/webapp/UserTest.java
@@ -0,0 +1,113 @@
+/**
+ * 
+ */
+package first.webapp;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author student
+ *
+ */
+class UserTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link first.webapp.User#User()}.
+	 */
+	@Test
+	void testUser() {
+		//fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for
+	 * {@link first.webapp.User#User(java.lang.String, java.lang.String, java.lang.String, java.lang.String)}.
+	 */
+	@Test
+	void testUserStringStringStringString() {
+		//fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link first.webapp.User#getName()}.
+	 */
+	@Test
+	void testGetName() {
+		//fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link first.webapp.User#setName(java.lang.String)}.
+	 */
+	@Test
+	void testSetName() {
+		//fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link first.webapp.User#getPassword()}.
+	 */
+	@Test
+	void testGetPassword() {
+		//fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link first.webapp.User#setPassword(java.lang.String)}.
+	 */
+	@Test
+	void testSetPassword() {
+		//fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link first.webapp.User#getEmail()}.
+	 */
+	@Test
+	void testGetEmail() {
+		//fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link first.webapp.User#setEmail(java.lang.String)}.
+	 */
+	@Test
+	void testSetEmail() {
+		//fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link first.webapp.User#getLanguage()}.
+	 */
+	@Test
+	void testGetLanguage() {
+		//fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link first.webapp.User#setLanguage(java.lang.String)}.
+	 */
+	@Test
+	void testSetLanguage() {
+		//fail("Not yet implemented");
+	}
+
+}
